a3e0b170ae4d60d230a7bcc4cf0d8cfedd03af26
hunksy <vfndtqxtvfuby@mail.ru>
2024-10-21 11:33:33 +0500

    Added recursive reverse number function

--- test-git/Program.cs
+++ test-git/Program.cs
@@ -12,9 +12,23 @@ namespace test_git
         {
             return a + b;
         }
+        static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Reverse(int number, int r)
+        {
+            if(number < 10)
+            {
+                return r*10+number;
+            }
+            return Reverse(number/10, r*10+number%10);
+        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine(Reverse(123,0));
             Console.WriteLine(Summary(1, 2));
             Console.WriteLine("hello");
         }
