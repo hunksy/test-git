01acaabdc52055182223f05f8ce1fac33bc483f8
hunksy <vfndtqxtvfuby@mail.ru>
2024-10-25 11:52:22 +0500

    Added Find number Function

--- test-git/Program.cs
+++ test-git/Program.cs
@@ -25,6 +25,30 @@ namespace test_git
             return a / b;
         }
 
+        static string FindNumber(int[,] array, int target, int row, int col)
+        {
+            //Если строка за пределами массива выводим что число не бло найдено
+            if (row >= array.GetLength(0)) 
+            {
+                return "Число не найдено";
+            }
+
+            //Если достигли конца строки, переходим на следующую строку
+            if (col >= array.GetLength(1)) 
+            {
+                return FindNumber(array, target, row + 1, 0);
+            }
+
+            //Если текущий элемент равен заданному числу выводим его позицию
+            if (array[row, col] == target)
+            {
+                return $"Число находится в {row+1} строке {col+1} столбца";
+            }
+
+            //Переход к следующему элементу в строке
+            return FindNumber(array, target, row, col + 1);
+        }
+
         static int Reverse(int number, int r)
         {
             if(number < 10)
@@ -36,9 +60,8 @@ namespace test_git
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Reverse(123,0));
-            Console.WriteLine(Summary(1, 2));
-            Console.WriteLine("hello");
+            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            Console.WriteLine(FindNumber(array, 8, 0, 0));
         }
     }
 }
